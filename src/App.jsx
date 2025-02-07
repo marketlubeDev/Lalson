ef1464ac4538035d017cc7b4e6433a36d36dc9f0
market lube <marketlubes@gmail.com>
2025-02-07 15:36:46 +0530

    grid section responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import { useState } from "react";
-
+import { useMediaQuery } from "react-responsive";
 import Grid from "./Page/Section2/Grid";
 import ImageSection from "./Page/Section1/ImageSection";
 import Scrollcard from "./Page/Section3/Scrollcard";
 import OurWork from "./Page/Section4/OurWork";
 import Curious from "./Page/Section5/Curious";
 import Footer from "./Page/Section6/Footer";
+import OurWorkMobile from "./Page/Section4/OurWorkMobo";
 import Nav from "./layout/Nav";
 
 function App() {
+  const isMobile = useMediaQuery({ maxWidth: 767 });
   return (
     <>
       {/* <Nav /> */}
       <ImageSection />
       <Grid />
       <Scrollcard />
-      <OurWork />
+      {isMobile ? <OurWorkMobile /> : <OurWork />}
       <Curious />
       <Footer />
     </>
---
+++ src/Page/Section4/OurWorkMobo.jsx
@@ -0,0 +1,77 @@
+function OurWorkMobile() {
+  return (
+    <section className="ourWorks">
+      <div className="ourWorks-container">
+        <h1 className="ourWorks-container-title">Our Works</h1>
+        <div className="grid-section" id="home">
+          <div
+            className="card"
+            style={{
+              gridRow: "span 12",
+            }}
+          ></div>
+
+          <div
+            className="card"
+            style={{
+              gridRow: "span 12",
+            }}
+          ></div>
+          <div
+            className="card"
+            style={{
+              gridRow: "span 12",
+            }}
+          ></div>
+
+          <div
+            className="card"
+            style={{
+              gridRow: "span 12",
+            }}
+          ></div>
+          <div
+            className="card"
+            style={{
+              gridRow: "span 12",
+            }}
+          ></div>
+
+          <div
+            className="card"
+            style={{
+              gridRow: "span 12",
+            }}
+          ></div>
+          <div
+            className="card"
+            style={{
+              gridRow: "span 12",
+            }}
+          ></div>
+          <div
+            className="card"
+            style={{
+              gridRow: "span 12",
+            }}
+          ></div>
+          <div
+            className="card"
+            style={{
+              gridRow: "span 12",
+            }}
+          ></div>
+
+          <div
+            className="card"
+            style={{
+              gridRow: "span 12",
+            }}
+          ></div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default OurWorkMobile;
