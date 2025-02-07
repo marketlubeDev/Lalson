b50017b0d8e9a06507f04243f0cd7b2a50820efe
market lube <marketlubes@gmail.com>
2025-02-07 15:03:41 +0530

    grid section as per the ui

--- src/Page/Section4/OurWork.jsx
+++ src/Page/Section4/OurWork.jsx
@@ -3,7 +3,73 @@ function OurWork() {
     <section className="ourWorks">
       <div className="ourWorks-container">
         <h1 className="ourWorks-container-title">Our Works</h1>
-        <div className="ourWorks-container-grid">grid</div>
+        {/* <div className="ourWorks-container-grid">grid</div> */}
+        <div className="grid-section" id="home">
+          <div
+            className="card"
+            style={{
+              gridRow: "span 10",
+            }}
+          ></div>
+
+          <div
+            className="card"
+            style={{
+              gridRow: "span 15",
+            }}
+          ></div>
+          <div
+            className="card"
+            style={{
+              gridRow: "span 10",
+            }}
+          ></div>
+
+          <div
+            className="card"
+            style={{
+              gridRow: "span 15",
+            }}
+          ></div>
+          <div
+            className="card"
+            style={{
+              gridRow: "span 10",
+            }}
+          ></div>
+
+          <div
+            className="card"
+            style={{
+              gridRow: "span 10",
+            }}
+          ></div>
+          <div
+            className="card"
+            style={{
+              gridRow: "span 15",
+            }}
+          ></div>
+          <div
+            className="card"
+            style={{
+              gridRow: "span 15",
+            }}
+          ></div>
+          <div
+            className="card"
+            style={{
+              gridRow: "span 10",
+            }}
+          ></div>
+
+          <div
+            className="card"
+            style={{
+              gridRow: "span 10",
+            }}
+          ></div>
+        </div>
       </div>
     </section>
   );
