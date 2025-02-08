b28c41399a0123b7aec116aae47a1784ab3575e4
market lube <marketlubes@gmail.com>
2025-02-08 14:33:51 +0530

    some changes and change the fonts of the project

--- src/Page/Section1/ImageSection.jsx
+++ src/Page/Section1/ImageSection.jsx
@@ -5,7 +5,13 @@ import Count from "./Count";
 function ImageSection() {
   return (
     <div className="imageSection">
-      <div></div>
+      <div className="banner-container">
+        <div className="text-center">
+          <h2 className="small-text1">Your</h2>
+          <h1 className="large-text">Audio Visual</h1>
+          <h2 className="small-text2">Partner</h2>
+        </div>
+      </div>
       <div className="imageSection-container">
         <div className="imageSection-container-left">
           <div className="imageSection-container-left-we">
