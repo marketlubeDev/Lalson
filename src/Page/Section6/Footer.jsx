c07693fb9f3fab6a3b5d1332abc6bbff72562e2a
market lube <marketlubes@gmail.com>
2025-02-08 10:57:50 +0530

    change footer components

--- src/Page/Section6/Footer.jsx
+++ src/Page/Section6/Footer.jsx
@@ -1,3 +1,6 @@
+import React from "react";
+import { FaFacebookF, FaInstagram } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
 import logo from "./../../assets/Lalisons.svg";
 function Footer() {
   return (
@@ -8,41 +11,46 @@ function Footer() {
             <img src={logo} alt="LALSONS Electronics" />
           </div>
         </div>
-
-        <div className="footer-center">
-          <nav className="footer-nav">
-            <a href="#home">Home</a>
-            <a href="#about">About</a>
-            <a href="#services">Services</a>
-            <a href="#projects">Projects</a>
-            <a href="#testimonial">Testimonial</a>
-            <a href="#contact" className="contact-btn">
-              Contact
-            </a>
-          </nav>
-        </div>
-
         <div className="footer-right">
-          <p className="footer-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor nibh euismod tincidunt ut. Lorem ipsum dolor sit
-            amet, consectetur adipiscing elit, sed do eiusmod tempor nibh
-            euismod tincidunt ut
-          </p>
-
-          <div>
+          <div className="footer-navbar">
+            <nav className="footer-nav">
+              <a href="#home">Home</a>
+              <a href="#about">About</a>
+              <a href="#services">Services</a>
+              <a href="#projects">Projects</a>
+              <a href="#testimonial">Testimonial</a>
+              <a href="#contact" className="contact-btn">
+                Contact
+              </a>
+            </nav>
+          </div>
+          <div className="footer-address">
+            <div className="">
+              <p className="footer-text">
+                Lorem ipsum dolor sit amet,
+                <br /> consectetuer adipiscing elit,
+                <br /> sed diam nonummy nibh
+                <br /> euismod tincidunt ut
+              </p>
+            </div>
             <div className="contact-info">
               <a href="mailto:hello@lalsons.com">hello@lalsons.com</a>
               <a href="tel:+919900099900">+91 9900 0999 9900</a>
               <div className="social-links">
-                <a href="#facebook" aria-label="Facebook">
-                  <div className="social-icon"></div>
+                <a href="https://www.facebook.com/" className="social-link">
+                  <div className="social-circle">
+                    <FaFacebookF />
+                  </div>
                 </a>
-                <a href="#twitter" aria-label="Twitter">
-                  <div className="social-icon"></div>
+                <a href="https://www.instagram.com/" className="social-link">
+                  <div className="social-circle">
+                    <FaInstagram />
+                  </div>
                 </a>
-                <a href="#instagram" aria-label="Instagram">
-                  <div className="social-icon"></div>
+                <a href="https://wa.me/1234567890" className="social-link">
+                  <div className="social-circle">
+                    <FaWhatsapp />
+                  </div>
                 </a>
               </div>
             </div>
@@ -52,7 +60,7 @@ function Footer() {
 
       <div className="footer-bottom">
         <p>
-          made by <a href="#marketlube">Marketlube</a>
+          made by <a href="https://www.marketlube.in/">Marketlube</a>
         </p>
       </div>
     </footer>
