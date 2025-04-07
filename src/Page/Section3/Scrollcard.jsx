269cbb5a54be36c73974e50760210a28d96b734c
marketLube <marketlubes@gmail.com>
2025-04-07 17:32:13 +0530

    SL

--- src/Page/Section3/Scrollcard.jsx
+++ src/Page/Section3/Scrollcard.jsx
@@ -9,15 +9,15 @@ const obj = [
   {
     img: IMG3,
     color: "#ffffff",
-    desc: "Enhancing sound quality with expert acoustic solutions for optimal performance",
-    title: "Acoustic Consultation",
+    desc: "Enhancing the acoustic characteristics of your space with expert acoustic solutions for quality sound reproduction and intelligibility",
+    title: "Acoustic Consultation and Designing.",
     span: "1",
     type: "green",
   },
   {
     img: IMG2,
     color: " #a6a6a6",
-    desc: "Providing end-to-end project design solutions for optimal sound and visuals.",
+    desc: "Providing end-to-end project design solutions for an optimal Audio Visual experience.",
     title: "Complete Project Designing",
     span: "2",
     type: "blue",
@@ -25,7 +25,7 @@ const obj = [
   {
     img: IMG4,
     color: "#ffffff",
-    desc: "Expert installation of high-quality equipment for superior sound and visuals.",
+    desc: "You can count on us to install your precious equipment's, so that they perform precisely as it is meant to be.",
     title: "Installation of Equipment",
     span: "3",
     type: "gray",
@@ -33,7 +33,7 @@ const obj = [
   {
     img: IMG1,
     color: " #a6a6a6",
-    desc: "Ensuring long-term performance with reliable annual maintenance and aftercare services.",
+    desc: "We will take care of all the products sold by us with our in house service department. AMC packages are also available ensuring you with peace of mind.",
     title: "Annual Maintenance & After care",
     span: "4",
     type: "blue",
